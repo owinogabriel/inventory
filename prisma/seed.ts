6d87496298c17b36ef929509d3e04e6760ecffc8
owinogabriel <g73346920@gmail.com>
2025-10-29 15:50:59 +0300

    feat:Created a sample product with a random price and quantity

---
+++ prisma/seed.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const demoUserId = "93cf4176-40a4-4e49-85b4-1ed7d3712c0c";
+
+  // Create sample products
+  await prisma.product.createMany({
+    data: Array.from({ length: 25 }).map((_, i) => ({
+      userId: demoUserId,
+      name: `Product ${i + 1}`,
+      price: (Math.random() * 90 + 10).toFixed(2),
+      quantity: Math.floor(Math.random() * 20),
+      lowStockAt: 5,
+      createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * (i * 5)),
+    })),
+  });
+
+  console.log("Seed data created successfully!");
+  console.log(`Created 25 Products for user ID: ${demoUserId}`);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
