b957bc23fab85c8a8119f62ac4bf68efd7cbe15b
owinogabriel <g73346920@gmail.com>
2025-11-01 16:35:19 +0300

    feat:imported link from next/link

--- app/add-product/page.tsx
+++ app/add-product/page.tsx
@@ -3,11 +3,12 @@ import { createProduct } from "@/lib/actions/products";
 import { getCurrentUser } from "@/lib/auth";
 import Link from "next/link";
 
-export default function AddProduct() {
-  const user = getCurrentUser();
+export default async function AddProductPage() {
+  const user = await getCurrentUser();
   return (
-    <div className="min-h-screen bg-gray">
+    <div className="min-h-screen bg-gray-50">
       <Sidebar currentPath="/add-product" />
+
       <main className="ml-64 p-8">
         <div className="mb-8">
           <div className="flex items-center justify-between">
@@ -16,14 +17,14 @@ export default function AddProduct() {
                 Add Product
               </h1>
               <p className="text-sm text-gray-500">
-                Add a new product to you inventory.
+                Add a new product to your inventory
               </p>
             </div>
           </div>
         </div>
 
         <div className="max-w-2xl">
-          <div className="bg-white rounded-lg border-gray-200 p-6">
+          <div className="bg-white rounded-lg border border-gray-200 p-6">
             <form className="space-y-6" action={createProduct}>
               <div>
                 <label
@@ -82,35 +83,37 @@ export default function AddProduct() {
 
               <div>
                 <label
-                  htmlFor="name"
+                  htmlFor="sku"
                   className="block text-sm font-medium text-gray-700 mb-2"
                 >
-                  SKU (optional) *
+                  SKU (optional)
                 </label>
                 <input
                   type="text"
-                  id="name"
-                  name="name"
+                  id="sku"
+                  name="sku"
                   className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:border-transparent"
                   placeholder="Enter SKU"
                 />
               </div>
+
               <div>
                 <label
                   htmlFor="lowStockAt"
                   className="block text-sm font-medium text-gray-700 mb-2"
                 >
-                  Low Stock At (optional) *
+                  Low Stock At (optional)
                 </label>
                 <input
                   type="number"
                   id="lowStockAt"
                   name="lowStockAt"
-                  required
+                  min="0"
                   className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:border-transparent"
                   placeholder="Enter low stock threshold"
                 />
               </div>
+
               <div className="flex gap-5">
                 <button
                   type="submit"
@@ -121,7 +124,9 @@ export default function AddProduct() {
                 <Link
                   href="/inventory"
                   className="px-6 py-3 bg-gray-200 text-gray-800 rounded-lg hover:bg-gray-300"
-                ></Link>
+                >
+                  Cancel
+                </Link>
               </div>
             </form>
           </div>
